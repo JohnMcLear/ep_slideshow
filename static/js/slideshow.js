59f08988e4155f550965ea96ba3618800688e5c0
John McLear <john@mclear.co.uk>
2013-03-01 21:57:08 +0000

    first version working :)

--- static/js/slideshow.js
+++ static/js/slideshow.js
@@ -1,53 +1,84 @@
-// PROBLEMS and TODO list
-// arrow keys dont work
-// no right click to go back a slide
-// need ability to go onto next and previous slide
-// headings prolly break line height issues
-
-
-
-
 var postAceInit = function(hook, context){
+  var currentPosition;
   var slideShow = {
     enable: function() {
       $('#editorcontainer, iframe, .menu_left, .menu_right').addClass('slideshow');
-      $('iframe[name="ace_outer"]').contents().find("#outerdocbody").css('background','transparent');
-      $('iframe[name="ace_outer"]').contents().find("#outerdocbody").css('overflow','hidden');
-      $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody").css('zoom','150%');
-      $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody").contents().find("h1").not(":eq(0)").css("padding-top","500px");
+      $('iframe[name="ace_outer"]').contents().find("#outerdocbody").css({'background':'transparent', 'overflow':'hidden'});
+      $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody").contents().find("h1").not(":eq(0)").css("margin-top","1000px");
+      $('iframe[name="ace_outer"]').contents().find("#outerdocbody").scrollTop(0); // go to 0 position (Start of presentation)
+      // make last line super high, this is hacky but it just means we have enough space to scrollto if its near the bottom of the document
+      $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody").contents().last("div").css("height","1000px");
+
+      currentPosition = 0; // go to start of document
 
       pad.changeViewOption('showLineNumbers', false);
       $(".popup").hide();
 
-      $("body").keypress(function(e) {
-        console.log(e);
-        console.warn("Handler for .keypress() called.");
-      });
-      $("body").click(function(e) {
-        if(e.target.id == "editorcontainer"){ // if we click on the main body
-          slideShow.next(); // go to next slide
+      $("body").keydown(function(e) {
+        if(e.keyCode == 39){ // next slide from right arrow
+          slideShow.next();
+        }else if(e.keyCode == 37){ // previous slide from left arrow
+          slideShow.previous();
         }
+      });
+      $("body").mousedown(function(e) {
+        if(e.target.id == "editorcontainer"){ // if we click on the main body
+          if(e.which == 1){
+            slideShow.next(); // go to next slide
+          }
+        }
+      });
+      $("body").bind("contextmenu", function(e){
+        e.preventDefault();
+        slideShow.previous(); // go to previous slide
+        return false;
       });
 
     },
-    disable: function() {
+    disable: function() { // disable the slideshow functionality
       $('#editorcontainer, iframe, .menu_left, .menu_right').removeClass('slideshow');
       $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody").css('zoom','100%');
-      $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody").contents().find("h1").not(":eq(0)").css("padding-top","0px");
+      $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody").contents().find("h1").not(":eq(0)").css("margin-top","0px");
+      $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody").contents().last("div").css("height","20px");
       $('iframe[name="ace_outer"]').contents().find("#outerdocbody").css('overflow','auto');
       pad.changeViewOption('showLineNumbers', true);
+
+      $("body").keydown(function(e) {
+        if(e.keyCode == 39){ // next slide from right arrow
+          return false;
+        }else if(e.keyCode == 37){ // previous slide from left arrow
+          return false;
+        }
+      });
+      $("body").mousedown(function(e) {
+        if(e.target.id == "editorcontainer"){ // if we click on the main body
+          if(e.which == 1){
+            return false;
+          }
+        }
+      });
+      $("body").bind("contextmenu", function(e){
+        return false;
+      });
+
     },
-    next: function(){
-      var status = slideShow.status(); // get the current slide number
-      currentSlide = status.slideNumber;
-      console.log(currentSlide);
+    next: function(){ // go to next slide
+      var targetH1 = currentPosition +1;
+      currentPosition = currentPosition +1;
+      var h1 = $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody").contents().find("h1").eq(targetH1); // get the target element
+      if(h1.offset()){ // if the element exists
+        var newY = h1.offset().top;
+        $('iframe[name="ace_outer"]').contents().find("#outerdocbody").scrollTop(newY);
+      }
     },
-    status: function(){
-      var state = {};
-      var html = $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody").html; // get the HTML
-      // console.log(html);
-      state.slideNumber = 0;
-      return state;
+    previous: function(){ // go to previous slide
+      var targetH1 = currentPosition -1;
+      currentPosition = currentPosition -1;
+      var h1 = $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody").contents().find("h1").eq(targetH1); // get the target element
+      if(h1.offset()){ // if the element exists
+        var newY = h1.offset().top;
+        $('iframe[name="ace_outer"]').contents().find("#outerdocbody").scrollTop(newY);
+      }
     }
   }
   /* init */
